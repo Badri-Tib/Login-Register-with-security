bccc4a610df8881c43389de2f55645567298671f
GHOST <badri.tibourtine@gmail.com>
2023-02-09 23:59:29 +0100

    First commit with hash made on client side

---
+++ pages/api/authentification.ts
@@ -0,0 +1,19 @@
+import path from 'path';
+import { promises as fs } from 'fs';
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type Data = {
+  name: string
+}
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<String>
+  ) {
+  //Find the absolute path of the json directory
+  const jsonDirectory = path.join(process.cwd(), 'json');
+  //Read the json data file data.json
+  const fileContents = await fs.readFile(jsonDirectory + '/users.json', 'utf8');
+  //Return the content of the data file in json format
+  res.status(200).json(fileContents);
+}
